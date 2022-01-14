e19e8ae0abf9a82d1f89f4aa722a1cc49d6a38b7
x <xxx@tsststs.com>
2022-01-14 00:04:52 -0500

    lab

---
+++ lab_1-1.py
@@ -0,0 +1,14 @@
+# MEE 01/06/21
+
+
+rows = [["Mateo", "Jason", "Jordan", "Mohamed", "Michael", "Charlie", "Declan"], ["Sean", "Luis", "Ryan", "James", "Jack"],
+["Aiden", "Ian", "Colin" "Tim", "Cam"],
+["Larry", "Spencer", "Chris", "Ryan", "Nolan"]]
+
+
+for row in rows:
+    for name in row:
+        if(name.endswith("s")):
+            print("{0}\'".format(name))
+        else:
+            print("{0}\'s".format(name))
---
+++ lab_1-3.py
@@ -0,0 +1,22 @@
+# MEE 01/10/22
+
+
+def add_foods(lst):
+    sixth_letter = []
+    not_foods = []
+    short_foods = []
+    for x in lst:
+        try:
+            sixth_letter.append(x[5])
+        except TypeError:
+            not_foods.append(x)
+        except IndexError:
+            short_foods.append(x)
+
+    print("sixth_letter: ", sixth_letter)
+    print("not_foods: ", not_foods)
+    print("short_foods: ", short_foods)
+
+add_foods(['potatoes', 'salsa', 'cherries', 'banana', 'apple'])
+add_foods(['naan', 'celery', 'buckwheat', 7, 'clementine'])
+add_foods(['cheesburger', True, 'chicken', 'rice', 'radish'])
